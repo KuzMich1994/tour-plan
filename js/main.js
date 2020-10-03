eca94fd80730096a4c4a833382cc732cbca1de2c
KuzMich1994 <drumer19940310@mail.ru>
2020-10-03 13:26:04 +0300

    refact: Redesigned for PP header and hotel

--- js/main.js
+++ js/main.js
@@ -45,9 +45,9 @@ $(document).ready(function () {
   // Отправка данных на сервер
   function send(event, php) {
     console.log("Отправка запроса");
-    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     var req = new XMLHttpRequest();
-    req.open('POST', php, true);
+    req.open("POST", php, true);
     req.onload = function () {
       if (req.status >= 200 && req.status < 400) {
         json = JSON.parse(this.response); // Ебанный internet explorer 11
@@ -62,31 +62,35 @@ $(document).ready(function () {
           alert("Ошибка. Сообщение не отправлено");
         }
         // Если не удалось связаться с php файлом
-      } else { alert("Ошибка сервера. Номер: " + req.status); }
+      } else {
+        alert("Ошибка сервера. Номер: " + req.status);
+      }
     };
 
     // Если не удалось отправить запрос. Стоит блок на хостинге
-    req.onerror = function () { alert("Ошибка отправки запроса"); };
+    req.onerror = function () {
+      alert("Ошибка отправки запроса");
+    };
     req.send(new FormData(event.target));
   }
 
-  let modalButton = $('[data-toggle="modal"]')
-  let closeModalButton = $('.modal__close')
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
+  let modalButton = $('[data-toggle="modal"]');
+  let closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  closeModalButton.on("keyup", closeModal);
 
   function openModal() {
-    let modalOverlay = $('.modal__overlay');
-    let modalDialog = $('.modal__dialog');
-    modalOverlay.addClass('modal__overlay_visible');
-    modalDialog.addClass('modal__dialog_visible');
+    let modalOverlay = $(".modal__overlay");
+    let modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay_visible");
+    modalDialog.addClass("modal__dialog_visible");
   }
   function closeModal(event) {
-    event.preventDefault()
-    let modalOverlay = $('.modal__overlay');
-    let modalDialog = $('.modal__dialog');
-    modalOverlay.removeClass('modal__overlay_visible');
-    modalDialog.removeClass('modal__dialog_visible');
+    event.preventDefault();
+    let modalOverlay = $(".modal__overlay");
+    let modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay_visible");
+    modalDialog.removeClass("modal__dialog_visible");
   }
-
 });
